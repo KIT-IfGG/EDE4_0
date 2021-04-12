a8645dee0f20f9dfef199e10db3a0a7b1fe8b75a
unknown <rasilgon@gmail.com>
2021-04-12 16:50:44 +0200

    Included all climate scenarios and bioclim for them

--- createDataBestand.R
+++ createDataBestand.R
@@ -55,9 +55,13 @@ createDataBestand <- function(i){
   df$site[df$site==germanSites[1]] <- bestandNanmes[i]
   dummyBestand$Klima_Hist <- df
   
-  df <- getData(dataset = "CLIMATE_ISIMIP2BLBC", site = germanSites[i])
-  df <- df[df$forcingCondition=="rcp2p6",]
-  df$site[df$site==germanSites[i]] <- bestandNanmes[i]
+  df <- getData(dataset = "CLIMATE_ISIMIP2BLBC", site = germanSites[i],collapse = FALSE )
+  for (k in 1:length(df)){
+    for (j in 1:length(df[[i]])){
+      df[[k]][[j]]$site[df[[k]][[j]]$site==germanSites[1]] <- bestandNanmes[i]
+      
+    }
+  }
   dummyBestand$Klima_Pro <- df
   
   return(dummyBestand)
@@ -105,8 +109,8 @@ calculateBioVarHist <- function(BestandObject){
                                            c(1:19), sep= ""))
   meanbioclimBestand <- apply(bioclimBestand,MARGIN = 2, mean)
   meanbioclimBestand <- meanbioclimBestand[2:length(meanbioclimBestand)]
-  BestandObject[["Bioclim_full"]] <- as.data.frame(bioclimBestand)
-  BestandObject[["Bioclim_"]] <- as.data.frame(meanbioclimBestand)
+  BestandObject[["Bioclim_full_hist"]] <- as.data.frame(bioclimBestand)
+  BestandObject[["Bioclim_mean_hist"]] <- as.data.frame(meanbioclimBestand)
   return(BestandObject)
 }
 
@@ -124,3 +128,63 @@ Bestand_C <- calculateBioVarHist(Bestand_C)
 saveRDS(Bestand_A, file = "E:/EDE_Daten/Bestand_A.rds")
 saveRDS(Bestand_B, file = "E:/EDE_Daten/Bestand_B.rds")
 saveRDS(Bestand_C, file = "E:/EDE_Daten/Bestand_C.rds")
+
+
+
+
+calculateBioVarHist.simple <- function(df_clima){
+ 
+  years <- unique(df_clima$year)
+  holder <- vector(mode = "list", length = length(years))
+  for (i in 1:length(years)){
+    df <- df_clima[df_clima$year== years[i], ]
+
+    temp <- aggregate(df[, c("mo", "tmax_degC", "tmean_degC", "tmin_degC")],
+                      FUN="mean", by = list(df$mo))
+    prec <- aggregate(df[, c("mo", "p_mm")],
+                      FUN="sum", by = list(df$mo))
+    biodf <- biovars(prec$p_mm, temp$tmin_degC, temp$tmax_degC)
+    bioRow <-  c(years[i], biodf)
+    names(bioRow) <- c("year", paste("bio", c(1:length(biodf))))
+    holder[[i]] <- c(years[i], biodf)
+  }
+  bioclimBestand <- do.call("rbind", holder)
+  colnames(bioclimBestand) <- c("year", paste("bio",c(1:19), sep= ""))
+    
+  return(bioclimBestand)
+}
+
+
+calculateBioVarHist.pro <- function(BestandObject){
+  dummy <- BestandObject[["Klima_Pro"]]
+  dummyholder <- dummy
+  
+  forcingCondition <- names(dummy)
+  for (i in  1:length(forcingCondition)){
+    rcp <- names(dummy[[i]])
+    rcp <- rcp[rcp %in% c("rcp2p6", "rcp4p5", "rcp6p0", "rcp8p5")]
+    for (j in 1:length(rcp)){
+      dummyholder[[i]][[j]] <-calculateBioVarHist.simple( dummy[[i]][[j]])
+      
+    }
+  }
+  BestandObject[["Bioclim_full_pro"]] <- dummyholder
+  return(BestandObject)
+
+}
+
+
+Bestand_A <- readRDS("E:/EDE_Daten/Bestand_A.rds")
+Bestand_B <- readRDS("E:/EDE_Daten/Bestand_B.rds")
+Bestand_C <- readRDS("E:/EDE_Daten/Bestand_C.rds")
+
+Bestand_A <- calculateBioVarHist.pro(Bestand_A)
+Bestand_B <- calculateBioVarHist.pro(Bestand_B)
+Bestand_C <- calculateBioVarHist.pro(Bestand_C)
+
+
+
+saveRDS(Bestand_A, file = "E:/EDE_Daten/Bestand_A.rds")
+saveRDS(Bestand_B, file = "E:/EDE_Daten/Bestand_B.rds")
+saveRDS(Bestand_C, file = "E:/EDE_Daten/Bestand_C.rds")
+
